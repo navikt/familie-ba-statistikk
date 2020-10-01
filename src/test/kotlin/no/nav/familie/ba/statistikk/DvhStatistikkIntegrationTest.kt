1a4bc79e3ceb60af47ed357831fa990ea17f36d5
Tor Nærland <tor.nerland@nav.no>
2020-10-01 14:19:20 +0200

    lagt til tester for lagring av saksstatistikk

--- src/test/kotlin/no/nav/familie/ba/statistikk/DvhStatistikkIntegrationTest.kt
+++ src/test/kotlin/no/nav/familie/ba/statistikk/DvhStatistikkIntegrationTest.kt
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException
 import io.mockk.every
 import io.mockk.mockk
 import no.nav.familie.ba.statistikk.database.DbContainerInitializer
+import no.nav.familie.ba.statistikk.domene.SaksstatistikkDvhRepository
 import no.nav.familie.ba.statistikk.domene.VedtakDvhRepository
-import no.nav.familie.eksterne.kontrakter.VedtakDVH
 import no.nav.familie.kontrakter.felles.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.junit.jupiter.api.Assertions
@@ -15,6 +15,8 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
@@ -33,15 +35,70 @@ class DvhStatistikkIntegrationTest {
     @Autowired
     lateinit var vedtakKafkaConsumer: VedtakKafkaConsumer
 
+    @Autowired
+    lateinit var saksstatistikkDvhRepository: SaksstatistikkDvhRepository
+
+    @Autowired
+    lateinit var saksstatistikkBehandlingConsumer: SaksstatistikkBehandlingConsumer
+
+    @Autowired
+    lateinit var sakConsumer: SaksstatistikkSakConsumer
+
+    @Autowired
+    lateinit var jdbcTemplate: NamedParameterJdbcTemplate
+
+
     private val ack: Acknowledgment = mockk(relaxed = true)
 
     @Test
     fun `consume() skal lagre vedtak til database`() {
-        val vedtak = TestData.vedtakDhv()
+        val vedtak = TestData.vedtakDvh()
 
         vedtakKafkaConsumer.consume(lagConsumerRecord(vedtak), ack)
 
         Assertions.assertTrue(vedtakDvhRepository.finnes(vedtak))
+    }
+
+    @Test
+    fun `skal lagre saksstatistikk til database`() {
+        val sak = TestData.sakDvh()
+        val parameters = MapSqlParameterSource().addValue("sakId", sak.sakId)
+
+        val init_count =
+                jdbcTemplate.queryForObject("select count(*) from SAKSSTATISTIKK_DVH where type='SAK' and JSON ->> 'sakId' = :sakId",
+                                            parameters,
+                                            Int::class.java)!!
+
+        sakConsumer.consume(lagConsumerRecord(sak), ack)
+
+        val count: Int =
+                jdbcTemplate.queryForObject("select count(*) from SAKSSTATISTIKK_DVH where type='SAK' and JSON ->> 'sakId' = :sakId",
+                                            parameters,
+                                            Int::class.java)!!
+
+        Assertions.assertTrue(count > init_count)
+
+    }
+
+    @Test
+    fun `skal lagre saksstatistikk for behandling til database`() {
+        val behandling = TestData.behandlingDvh()
+        val parameters = MapSqlParameterSource().addValue("behandlingId", behandling.behandlingId)
+
+        val init_count =
+                jdbcTemplate.queryForObject("select count(*) from SAKSSTATISTIKK_DVH where type='BEHANDLING' and JSON ->> 'behandlingId' = :behandlingId",
+                                            parameters,
+                                            Int::class.java)!!
+
+        saksstatistikkBehandlingConsumer.consume(lagConsumerRecord(behandling), ack)
+
+        val count: Int =
+                jdbcTemplate.queryForObject("select count(*) from SAKSSTATISTIKK_DVH where type='BEHANDLING' and JSON ->> 'behandlingId' = :behandlingId",
+                                            parameters,
+                                            Int::class.java)!!
+
+        Assertions.assertTrue(count > init_count)
+
     }
 
     @Test
@@ -51,7 +108,7 @@ class DvhStatistikkIntegrationTest {
 
          Assertions.assertThrows(IllegalStateException::class.java) {
              VedtakKafkaConsumer(repository)
-                     .consume(lagConsumerRecord(TestData.vedtakDhv()), ack)
+                     .consume(lagConsumerRecord(TestData.vedtakDvh()), ack)
          }
 
 
@@ -80,7 +137,7 @@ class DvhStatistikkIntegrationTest {
 
     @Test
     fun `lagre() returverdi skal telle duplikater`() {
-        val vedtak = TestData.vedtakDhv()
+        val vedtak = TestData.vedtakDvh()
         val vedtakJson = objectMapper.writeValueAsString(vedtak)
         Assertions.assertEquals(1, vedtakDvhRepository.lagre(1, vedtak.behandlingsId, vedtakJson))
         Assertions.assertEquals(2, vedtakDvhRepository.lagre(1, vedtak.behandlingsId, vedtakJson))
@@ -91,7 +148,7 @@ class DvhStatistikkIntegrationTest {
 
     @Test
     fun `lagre() skal kaste feil hvis innhold i melding ikke er json`() {
-        val vedtak = TestData.vedtakDhv()
+        val vedtak = TestData.vedtakDvh()
         val vedtakJson = "FOO"
         Assertions.assertThrows(DataIntegrityViolationException::class.java) {
             vedtakDvhRepository.lagre(1, vedtak.behandlingsId, vedtakJson)
@@ -99,8 +156,8 @@ class DvhStatistikkIntegrationTest {
     }
 
 
-    private fun lagConsumerRecord(vedtak: VedtakDVH): ConsumerRecord<String, String> {
-        return ConsumerRecord("topic", 1, 1, vedtak.behandlingsId, objectMapper.writeValueAsString(vedtak))
+    private fun lagConsumerRecord(obj: Any): ConsumerRecord<String, String> {
+        return ConsumerRecord("topic", 1, 1, "1", objectMapper.writeValueAsString(obj))
     }
 
     private fun lagConsumerRecord(vedtak: String): ConsumerRecord<String, String> {
