e1736799e7299b3eac1ea4dcca78f4620571ddf2
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-05-11 12:45:35 +0200

    Leser melding selv om melding ikke validerer

--- src/main/kotlin/no/nav/familie/ba/statistikk/SaksstatistikkBehandlingConsumer.kt
+++ src/main/kotlin/no/nav/familie/ba/statistikk/SaksstatistikkBehandlingConsumer.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ba.statistikk
 
+import com.fasterxml.jackson.databind.JsonNode
 import no.nav.familie.ba.statistikk.domene.SaksstatistikkDvhRepository
 import no.nav.familie.eksterne.kontrakter.saksstatistikk.BehandlingDVH
 import no.nav.familie.kontrakter.felles.objectMapper
@@ -27,17 +28,26 @@ class SaksstatistikkBehandlingConsumer(private val saksstatistikkDvhRepository: 
             secureLogger.info("[${BEHANDLING}] Melding mottatt. offset=${cr.offset()}, key=${cr.key()}, melding=${cr.value()}")
 
             val json = cr.value()
+            val parent: JsonNode = objectMapper.readTree(json)
+            val funksjonellId: String = parent.path("funksjonellId").asText()
 
-            //valider at meldingen lar seg deserialisere
-            val behandlingDVH = objectMapper.readValue(json, BehandlingDVH::class.java)
-            saksstatistikkDvhRepository.lagre(BEHANDLING, cr.offset(), json, behandlingDVH.funksjonellId).apply {
+
+            saksstatistikkDvhRepository.lagre(BEHANDLING, cr.offset(), json, funksjonellId).apply {
                 when {
                     this == 1 -> secureLogger.info("Saksstatistikk-behandling mottatt og lagret: $json")
                     this > 1 -> logger.error("Saksstatistikk-behandling mottatt på nytt. Lagret, merket som duplikat. offset=${cr.offset()} key=${cr.key()}")
                     else -> throw error("Lagring av nytt Saksstatistikk-behandling mislyktes! offset=${cr.offset()} key=${cr.key()}")
                 }
             }
+            //valider at meldingen lar seg deserialisere
+            try {
+                objectMapper.readValue(json, BehandlingDVH::class.java)
+            } catch (e: Exception) {
+                logger.error("json for kan ikke parses til nyeste BehandlingDVH", e)
+                secureLogger.error("json for kan ikke parses til nyeste BehandlingDVH \n$json")
+            }
             validerBehandlingDvhMotJsonSchema(json)
+
 
 
         } catch (up: Exception) {
