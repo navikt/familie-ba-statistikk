41a11767eb8f86c53aff4bce4c897bde864237de
Tor Nærland <tor.nerland@nav.no>
2021-08-05 16:13:29 +0200

    Setter application/json som content type i response-header

--- src/main/kotlin/no/nav/familie/ba/statistikk/rest/SaksstatistikkController.kt
+++ src/main/kotlin/no/nav/familie/ba/statistikk/rest/SaksstatistikkController.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ba.statistikk.rest
 
 import no.nav.familie.ba.statistikk.domene.SaksstatistikkDvhRepository
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,12 +14,12 @@ import org.springframework.web.bind.annotation.RestController
 @ProtectedWithClaims(issuer = "azuread")
 class SaksstatistikkController(val saksstatistikkDvhRepository: SaksstatistikkDvhRepository) {
 
-    @GetMapping("/sak/{offset}")
+    @GetMapping(path = ["/sak/{offset}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun sak(@PathVariable offset: Long) : String {
         return saksstatistikkDvhRepository.hent("SAK", offset)
     }
 
-    @GetMapping("/behandling/{offset}")
+    @GetMapping(path = ["/behandling/{offset}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun behandling(@PathVariable offset: Long): String {
         return saksstatistikkDvhRepository.hent("BEHANDLING", offset)
     }
