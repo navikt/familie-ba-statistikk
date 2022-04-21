bdf78409f26a7e45fe77a210322b384a6005224e
Stig Strøm <stig.helge.strom@nav.no>
2022-04-21 13:58:13 +0200

    Swagger (#356)

---
+++ src/main/kotlin/no/nav/familie/ba/statistikk/config/SwaggerConfig.kt
@@ -0,0 +1,55 @@
+package no.nav.familie.ba.statistikk.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.OAuthFlow
+import io.swagger.v3.oas.models.security.OAuthFlows
+import io.swagger.v3.oas.models.security.Scopes
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig(@Value("\${AUTHORIZATION_URL}")
+                    val authorizationUrl: String,
+                    @Value("\${AZURE_OPENID_CONFIG_TOKEN_ENDPOINT}")
+                    val tokenUrl: String,
+                    @Value("\${API_SCOPE}")
+                    val apiScope: String) {
+
+    @Bean
+    fun openApi(): OpenAPI {
+        return OpenAPI().info(Info().title("BA Statistikk API"))
+            .components(Components()
+                            .addSecuritySchemes("oauth2", oauth2SecurityScheme())
+                            .addSecuritySchemes("bearer", bearerTokenSecurityScheme()))
+            .addSecurityItem(SecurityRequirement().addList("oauth2", listOf("read", "write")))
+            .addSecurityItem(SecurityRequirement().addList("bearer", listOf("read", "write")))
+    }
+
+    private fun oauth2SecurityScheme(): SecurityScheme {
+        return SecurityScheme()
+            .name("oauth2")
+            .type(SecurityScheme.Type.OAUTH2)
+            .scheme("oauth2")
+            .`in`(SecurityScheme.In.HEADER)
+            .flows(OAuthFlows()
+                       .authorizationCode(OAuthFlow().authorizationUrl(authorizationUrl)
+                                              .tokenUrl(tokenUrl)
+                                              .scopes(Scopes().addString(apiScope, "read,write"))))
+
+    }
+
+    private fun bearerTokenSecurityScheme(): SecurityScheme {
+        return SecurityScheme()
+            .type(SecurityScheme.Type.APIKEY)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER)
+            .name("Authorization")
+    }
+
+}
--- src/main/kotlin/no/nav/familie/ba/statistikk/rest/SaksstatistikkController.kt
+++ src/main/kotlin/no/nav/familie/ba/statistikk/rest/SaksstatistikkController.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ba.statistikk.rest
 
 import no.nav.familie.ba.statistikk.domene.SaksstatistikkDvhRepository
+import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
@@ -22,5 +23,15 @@ class SaksstatistikkController(val saksstatistikkDvhRepository: SaksstatistikkDv
     @GetMapping(path = ["/behandling/{offset}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun behandling(@PathVariable offset: Long): String {
         return saksstatistikkDvhRepository.hent("BEHANDLING", offset)
+    }
+
+    @GetMapping(path = ["/sak/fagsakid/{fagsakId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun harSendtMeldingForFagsak(@PathVariable fagsakId: String) : Ressurs<Boolean> {
+        return Ressurs.success(saksstatistikkDvhRepository.antallFagsakMedId(fagsakId) > 0)
+    }
+
+    @GetMapping(path = ["/sak/behandlingid/{behandlingId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun harSendtMeldingForBehandling(@PathVariable behandlingId: String) : Ressurs<Boolean> {
+        return Ressurs.success(saksstatistikkDvhRepository.antallBehandlingerMedId(behandlingId) > 0)
     }
 }
